701fdbbbd99443a6c66c39f5c50313e535064cc4
GuillaumeIfa <ory.guillaume@stagiairesifa.fr>
2019-02-15 16:45:58 +0100

    Page Inscription finie

--- inscription.php
+++ inscription.php
@@ -13,92 +13,91 @@
 		</p>
 		<form action="inscription.php" name="form1" method="POST">
 			<label for="pseudo">Pseudo:</label><br>
-			<input type="text" name='pseudo'><br>
+			<input type="text" name='pseudo' required><br>
 			<label for="address">Adresse:</label><br>
 			<input type="text" name="address"><br>
 			<label for="email">Email</label><br>
-			<input type="mail" name="email"><br>
+			<input type="email" name="email"><br>
 			<label for="phone">Téléphone</label><br>
 			<input type="tel" name="phone"><br>
 			<label for="password">Mot De Passe:</label><br>
-			<input type="password" name="password1"><br>
+			<input type="password" name="password1" required><br>
 			<label for="password">Confirmer Mot De Passe:</label><br>
-			<input type="password" name="password2"><br>
+			<input type="password" name="password2" required><br>
 
 			<input type="submit" name="submit1">
 		</form>
 	</section>
 
 	<section>
-	<h1>Connection Base De Données</h1>
+
 		<?php 
 
 			require_once './configure.php';
+			include 'functions.php';
 
 			$db_handle = mysqli_connect(DB_SERVER, DB_USER, DB_PASS);
 
 			$db_name = 'cinefa';
 			$db = mysqli_select_db($db_handle, $db_name);
+			$password1 = '';
+			$password2 = '';
 
-/*			$pseudo = '';
-			$address = '';
-			$email = '';
-			$phone = '';
-			$password = '';*/
-
-			if (isset($_POST['submit1']) && ($_POST['password1'] == $_POST['password2']))
+			if (isset($_POST['submit1']))
 			{
-				$pseudo = $_POST['pseudo'];
-				$address = $_POST['address'];
-				$email = $_POST['email'];
-				$phone = $_POST['phone'];
-				define('avant', 'caramelmou');
-				define('apres', 'chocopete');
-
-				$password = sha1(avant.$_POST['password1'].apres);
-
-			
-				if($db)
+				if ($_POST['password1'] != $_POST['password2'])
 				{
-					echo 'La base de données ' .$db_name. ' à bien été trouvée !<br>';
+					echo "Les mots de passe ne correspondent pas ¯\_(ツ)_/¯<br>";
+				}
+				else
+				{
 
 
-					$rqt_pseudo = 'SELECT * FROM USERS WHERE pseudo = "' .$pseudo. '";';
-					$result_query = mysqli_query($db_handle, $rqt_pseudo);
-					$db_field = mysqli_fetch_assoc($result_query);
-					if ($db_field)
+					$pseudo = trimmer($_POST['pseudo']);
+					$address = $_POST['address'];
+					$email = $_POST['email'];
+					$phone = $_POST['phone'];
+					define('avant', 'caramelmou');      // Definition Variable de Sécurité
+					define('apres', 'chocopete');		// Definition Variable de Sécurité
+
+					$password = sha1(avant.$_POST['password1'].apres);   // Hashage du MDP
+
+				
+					if($db)
 					{
-						echo '<b>Ce pseudo existe déjà !</b>';
-					}
-					else
-					{
+						/*echo 'La base de données ' .$db_name. ' à bien été trouvée !<br>';*/
 
-						$rqt = 
-						'
-							INSERT INTO USERS 
-							(
-							pseudo, address, email, phone, password
-							)
-							VALUES 
-							("'.$pseudo.'","'.$address.'","'.$email.'","'.$phone.'","'.$password.'");
-						';
 
-						$result_query = mysqli_query($db_handle, $rqt);
+						$rqt_pseudo = 'SELECT * FROM USERS WHERE pseudo = "' .$pseudo. '";';
+						$result_query = mysqli_query($db_handle, $rqt_pseudo);   // Ouvre la requete
+						$db_field = mysqli_fetch_assoc($result_query);			// Lit dans la table
 
-						if ($result_query) 
+						if ($db_field)
 						{
-							echo '<i>Vous êtes inscrit !</i>';
+							echo '<b>Ce pseudo existe déjà !</b>';
+						}
+						else
+						{
+							$rqt = 
+							'
+								INSERT INTO USERS 
+								(
+								pseudo, address, email, phone, password
+								)
+								VALUES 
+								("'.$pseudo.'","'.$address.'","'.$email.'","'.$phone.'","'.$password.'");
+							';
+
+							$result_query = mysqli_query($db_handle, $rqt);
+
+							if ($result_query) 
+							{
+								echo '<i>Vous êtes inscrit !</i>';
+							}
 						}
 					}
-
-				}
+				}	
 			}
-			else
-			{
-				echo 'Les mots de passe ne correspondent pas :/';
-			}
-
-
 		 ?>
 	</section>
 </body>
